3a797eedf6a9e04dc0b8809959330275e668a7ed
Peter Ritchie <peterritchie@quickenloans.com>
2017-03-09 15:56:10 -0500

    nuget support

---
+++ PRI.CallerInfo/CallerLineNumberAttribute.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable UnusedMember.Global
+namespace System.Runtime.CompilerServices
+{
+	/// <summary>
+	/// Allows you to obtain the line number in the source file at which the method is called.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Parameter)]
+	public class CallerLineNumberAttribute : Attribute
+	{
+	}
+}
